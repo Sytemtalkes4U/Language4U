6dfb944d84e8687959a9b9c113f97ded9ec7616b
Max Hernandez <hernandez.t.joshuamax@gmail.com>
2023-11-12 19:32:24 -0600

    dart upgrade dependecia

--- build/rive_common/intermediates/cxx/Debug/3e696x3d/logs/arm64-v8a/build_command_rive_text.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Mats\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Mats\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\rive_common-0.1.0\\android\\.cxx\\Debug\\3e696x3d\\arm64-v8a" ^
-  rive_text
